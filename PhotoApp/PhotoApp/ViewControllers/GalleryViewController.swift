d299463fb666b3687fb9ebba5325356ecc1d7d1e
soscooter <lelya.rumynin@gmail.com>
2023-08-12 19:35:23 +0300

    UIImageView

--- PhotoApp/PhotoApp/ViewControllers/GalleryViewController.swift
+++ PhotoApp/PhotoApp/ViewControllers/GalleryViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class GalleryViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
     }
 
@@ -19,8 +21,57 @@ class GalleryViewController: UIViewController {
     }
     
     @IBAction func plusButton(_ sender: Any) {
+        showAlerts()
+    }
+    
+    private func showAlerts(){
+        
+        let alert = UIAlertController(title: "Add photo", message: "Choose app for adding photo", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { [weak self] _ in
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.allowsEditing = false
+            pickerController.mediaTypes = ["public.image"]
+            pickerController.sourceType = .camera
+            self?.present(pickerController, animated: true)
+        }
+        
+        let galleryAction = UIAlertAction(title: "Gallery", style: .default) { [weak self] _ in
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.allowsEditing = false
+            pickerController.mediaTypes = ["public.image"]
+            pickerController.sourceType = .photoLibrary
+            self?.present(pickerController, animated: true)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addAction(cameraAction)
+        alert.addAction(galleryAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true)
         
     }
     
 
+}
+
+extension GalleryViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]){
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        self.imageView.image = image
+        picker.dismiss(animated: true)
+    }
+    
 }
